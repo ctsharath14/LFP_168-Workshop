4c0ec209401534d72a8f3e3d1675d48ba31c022a
ctshara14 <ctsharath14@gmail.com>
2022-07-13 21:59:03 +0530

    Adding: Fibonacci Series problem

---
+++ src/com/bridgelabz/FibonacciSeries.java
@@ -0,0 +1,24 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number upto which you need the Fibonacci Series");
+        int n = sc.nextInt();
+
+        System.out.println("The fibonacci series is :");
+        System.out.print(a + " ");
+
+        for (int i =1; i<n+1; i++) {
+            int c = a + b;
+            System.out.print(b+ " ");
+            a = b;
+            b = c;
+        }
+
+    }
+}
